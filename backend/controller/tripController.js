322f83f82cce7f7529b064c5f129954117061004
AMRUT SAGAR V <amrutsagar06@gmail.com>
2023-09-27 14:05:41 +0530

    function for create trip

--- backend/controller/tripController.js
+++ backend/controller/tripController.js
@@ -1,1 +1,24 @@
+const createTrip = async (req, res) => {
+    try {
+      const { tripName, destination, startDate, endDate } = req.body;
+      const userId = req.user._id;
   
+      const trip = new Trip({
+        tripName,
+        destination,
+        startDate,
+        endDate,
+        user: userId,
+      });
+  
+      const result = await trip.save();
+      res.status(201).json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Could not create trip' });
+    }
+  };
+  
+module.exports={
+    createTrip,
+}
